0aef5671b3567223c6fcf8764e5213d34be426e8
Azim <ammtkrmv@gmail.com>
2022-10-25 19:22:07 +0600

    Добавьте файлы проекта.

---
+++ StudentManager.Backend/Repositories/FileRepository.cs
@@ -0,0 +1,31 @@
+﻿using StudentManager.Model.Entities;
+
+namespace StudentManager.Model;
+
+public class FileRepository<T> : IRepository<T> where T : BaseEntity
+{
+    public T Create(T entity)
+    {
+        
+    }
+    public List<T> ReadAll()
+    {
+
+    }
+    public T ReadById(int id)
+    {
+
+    }
+    public T Update(T entity)
+    {
+
+    }
+    public void Delete(T entity)
+    {
+
+    }
+    public void DeleteById(int id)
+    {
+
+    }
+}
